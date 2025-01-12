2d58afc46c207b94c8d59e8f2f04b8e7cc4f1269
05-rahul-vishwakarma <rahul930vishwakarma@gmail.com>
2025-01-13 01:23:54 +0530

    update

--- next.config.js
+++ next.config.js
@@ -1,12 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    reactStrictMode: true,
-    images: {
-        domains: ['i.ibb.co','www.google.com','i.pinimg.com','th.bing.com'],
-        // Add your allowed external image domains here
-      },
-        domains: ['www.google.com', 'i.ibb.co'],
-    },
-}
+  reactStrictMode: true,
+  images: {
+    domains: ['i.ibb.co', 'www.google.com', 'i.pinimg.com', 'th.bing.com'],
+    // Add your allowed external image domains here
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
