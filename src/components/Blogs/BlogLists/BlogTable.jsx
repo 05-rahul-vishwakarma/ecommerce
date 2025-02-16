782bca9529a67fc02bdaf5b576ab5299a2519a43
05-rahul-vishwakarma <rahul930vishwakarma@gmail.com>
2025-02-16 15:27:17 +0530

    delete blog

--- src/components/Blogs/BlogLists/BlogTable.jsx
+++ src/components/Blogs/BlogLists/BlogTable.jsx
@@ -1,9 +1,26 @@
 "use client";
-import React from "react";
+import axios from "axios";
+import React, { useState } from "react";
 
 const BlogTable = ({ data }) => {
-    console.log(data, 'data');
-    
+    const [datas, setData] = useState(data); // Manage data with state
+
+    const handleDelete = async (PK, SK) => {
+        try {
+            const encodedPK = encodeURIComponent(PK);
+            const encodedSK = encodeURIComponent(SK);
+
+            const response = await axios.delete(
+                `${process.env.NEXT_PUBLIC_API_URL}/meta-content/blog/delete?PK=${encodedPK}&SK=${encodedSK}`,
+            );
+            setData((prevData) =>
+                prevData.filter((item) => item.PK !== PK || item.SK !== SK),
+            );
+        } catch (error) {
+            console.error("Error deleting item:", error);
+        }
+    };
+
     return (
         <div className="container mx-auto p-4">
             <h1 className="mb-4 text-2xl font-bold">Blog Table</h1>
@@ -20,7 +37,7 @@ const BlogTable = ({ data }) => {
                         </tr>
                     </thead>
                     <tbody>
-                        {data.map((blog, index) => (
+                        {datas?.map((blog, index) => (
                             <tr key={blog.PK} className="transition-colors duration-300 hover:bg-gray-100 dark:bg-[#0000002a] dark:shadow-card dark:hover:bg-gray-600">
                                 <td className="px-4 py-2 text-center">{index + 1}</td>
                                 <td className="px-4 py-2 capitalize ">{blog.title}</td>
@@ -32,7 +49,7 @@ const BlogTable = ({ data }) => {
                                         <button className="rounded bg-blue-500 px-2 py-1 text-white hover:bg-blue-600">
                                             View
                                         </button>
-                                        <button className="rounded bg-red-500 px-2 py-1 text-white hover:bg-red-600">
+                                        <button onClick={() => handleDelete(blog?.PK, blog?.SK)} className="rounded bg-red-500 px-2 py-1 text-white hover:bg-red-600">
                                             Delete
                                         </button>
                                     </div>
