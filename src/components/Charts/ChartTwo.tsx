8a8c2777a7ed04441675d5971a08de491e073720
05-rahul-vishwakarma <rahul930vishwakarma@gmail.com>
2025-02-19 03:26:31 +0530

    update

--- src/components/Charts/ChartTwo.tsx
+++ src/components/Charts/ChartTwo.tsx
@@ -81,7 +81,7 @@ const ChartTwo: React.FC = () => {
       fontSize: "14px",
 
       markers: {
-        radius: 99,
+        size: 99,
         // width: 16,
         // height: 16,
         strokeWidth: 10,
