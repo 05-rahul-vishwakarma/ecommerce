d35e10d7be5a4e954a8d9b8a1a13d7f256314f44
05-rahul-vishwakarma <rahul930vishwakarma@gmail.com>
2025-04-06 02:15:18 -0700

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,7 @@ import React from "react";
 export const metadata: Metadata = {
   title:
     "Next.js E-commerce Dashboard Page | NextAdmin - Next.js Dashboard Kit",
-  description: "This is Next.js Home page for NextAdmin Dashboard Kit",
+  description: "This is Next.js Home page for NextAdmin Dashboard",
 };
 
 export default function Home() {
