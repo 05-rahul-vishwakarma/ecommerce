1cc4f29beb9516e18e32b716108bfa3687b68c9c
05-rahul-vishwakarma <rahul930vishwakarma@gmail.com>
2025-04-14 14:37:38 -0700

    update

--- src/app/blog/[blog]/page.jsx
+++ src/app/blog/[blog]/page.jsx
@@ -1,25 +1,33 @@
+import Footer from "@/components/Footer/Footer";
+import MenuFour from "@/components/Header/MenuFour";
+import BlogDyanamicPage from "../../../components/Blog/BlogContent";
 import { blogListDataByPkSk } from "@/api/blogApis/getBlog";
-
 export default async function page({ params }) {
     const slug = (await params).blog;
     const decodedString = decodeURIComponent(slug);
     const param = new URLSearchParams(decodedString);
     const id = param.get("id");
     const id2 = param.get("id2");
-
+  
     const fetcher = await blogListDataByPkSk(id, id2);
-
-    if (!fetcher || !fetcher.data || fetcher?.data?.length === 0) {
-        return <div>No blog data found.</div>;
+    const blog = fetcher?.data?.[0];
+  
+    if (!blog) {
+      return (
+        <div className="min-h-screen flex items-center justify-center">
+          <p className="text-xl text-gray-500">Blog not found.</p>
+        </div>
+      );
     }
-
+  
     return (
-        <>
-            <div id="header" className="relative w-full">
-                <MenuFour props="bg-transparent" />
-            </div>
-            <BlogDyanamicPage data={fetcher?.data[0]} />
-            <Footer />
-        </>
+      <>
+        <div id="header" className="relative w-full">
+          <MenuFour props="bg-transparent" />
+        </div>
+        <BlogDyanamicPage data={blog} />
+        <Footer />
+      </>
     );
-}
+  }
+  
